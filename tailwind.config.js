1c8c1c43a3ddc6aa560036215e1dcb0b2675fbf3
Sarah Zhao <110712068+sarahlz01@users.noreply.github.com>
2025-01-20 00:07:39 -0800

    Add files via upload
    Signed-off-by: Sarah Zhao <110712068+sarahlz01@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-pink': '#f7e6ed',
+      },
+    },
+  },
+  plugins: [],
+};
+
