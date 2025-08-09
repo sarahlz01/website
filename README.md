735aa357bf36474b5a8e40d413972e8fdf77c196
Sarah Zhao <110712068+sarahlz01@users.noreply.github.com>
2025-08-09 11:59:29 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# website
-my website :D
+my personal website :D
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-pink': '#f7e6ed',
+      },
+    },
+  },
+  plugins: [],
+};
