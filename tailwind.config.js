55871766fe58a5ee5950c11af83477ae9654e796
Sarah Zhao <sarahzhao01@gmail.com>
2025-08-22 15:21:16 -0700

    added gradient background to home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       colors: {
         'custom-pink': '#f7e6ed',
       },
+      keyframes: {
+        gradient: {
+          "0%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+          "100%": { "background-position": "0% 50%" },
+        },
+      },
+      animation: {
+        gradient: "gradient 15s ease infinite",
+      },
     },
   },
   plugins: [],
